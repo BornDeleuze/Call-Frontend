ea4d216c115dba2971d9fd63f10d22fd03f5e3a7
Matt Beaudoin <ehxtwo@gmail.com>
2021-07-12 08:34:47 -0700

    basic skeleton start
    basic skeleton start!

---
+++ src/Conversation.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Messages from './Messages';
+
+function Conversations() {
+  return (
+    <div>
+      <Messages/>
+      <Messages/>
+      
+        
+      
+    </div>
+  );
+}
+
+export default Conversations;
---
+++ src/Messages.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Messages() {
+  return (
+    <div>
+      <h4>here's some messages</h4>
+      
+    </div>
+  );
+}
+
+export default Messages;
